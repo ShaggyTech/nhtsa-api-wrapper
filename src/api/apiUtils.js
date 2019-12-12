f29db7a80b57179f3be6c658ae4f80d8459b3f23
ShaggyTech <beedat1@gmail.com>
2019-12-12 17:06:13 -0600

    apiUtils.js: added documentation

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,7 +1,15 @@
+/******************************
+ * Api Utility/Helper Methods
+ *******************************/
+
 /**
- *
+ * Returns a url used to fetch data from the NHTSA.gov API
+ * @alias module:apiUtils.generateUrl
  * @param {string} vin VIN to decode
- * @param {string} apiEndpoint nhtsa.gov api to request from
+ * @param {string=} apiEndpoint=DecodeVinValuesExtended Endpoint of the NHTSA.gov API to request from
+ * @param {string=} responseFormat=json Requested response format from NHTSA.gov API call
+ * @returns {string} url ex: https://vpic.nhtsa.dot.gov/api/vehicles/{apiEndpoint}/{vin}?format={responseFormat}
+ *
  */
 const generateUrl = (vin, apiEndpoint, responseFormat) => {
   // Gatekeeping
@@ -9,19 +17,26 @@ const generateUrl = (vin, apiEndpoint, responseFormat) => {
     return { errMsg: 'api.generateUrl(): invalid argument (vin)' }
   }
 
-  // NHTSA.gov API Base Url
-  const baseUrl =
-    process.env.API_BASE_URL || 'https://vpic.nhtsa.dot.gov/api/vehicles'
-
   // API url options
   apiEndpoint =
     apiEndpoint || process.env.DEFAULT_API_ENDPOINT || 'DecodeVinValuesExtended'
   responseFormat =
     responseFormat || process.env.DEFAULT_API_RESPONSE_FORMAT || 'json'
 
+  // NHTSA.gov API Base Url
+  const baseUrl =
+    process.env.API_BASE_URL || 'https://vpic.nhtsa.dot.gov/api/vehicles'
+
   return `${baseUrl}/${apiEndpoint}/${vin}?format=${responseFormat}`
 }
 
+/**
+ * Handles any axios errors or custom errors from the api response
+ * @alias module:apiUtils.handleResponseError
+ * @param {any} error Response error to handle
+ * @returns {Error} Either the modified axios response or the original error
+ *
+ */
 const handleResponseError = error => {
   if (error.response) {
     // The request was made and the server responded with a status code
@@ -46,5 +61,9 @@ const handleResponseError = error => {
   }
 }
 
-exports.generateUrl = generateUrl
-exports.handleResponseError = handleResponseError
+/**
+ * Api Utility/Helper Methods
+ * @module apiUtils
+ */
+module.exports.generateUrl = generateUrl
+module.exports.handleResponseError = handleResponseError
