a639d25ea2e0cea4934d8672138092d1f10ca2fe
ShaggyTech <beedat1@gmail.com>
2020-01-14 23:53:44 -0600

    queryString renamed to makeQueryString, tests updated, small refactor

--- src/utils/makeQueryString.ts
+++ src/utils/makeQueryString.ts
@@ -1,29 +1,43 @@
-// Param union type
-type Param = string | number;
-
-export interface Params {
-  [propName: string]: Param;
+/**
+ * Object containing Key:Value pairs to build the URL query string with.<br>
+ * Parameter values may be either strings or numbers. <br>
+ * ---
+ *
+ * @category utils
+ * @interface
+ * @see [utils/makeQueryString](module-utils_makeQueryString.html)
+ * @example
+ * {
+ *   format: 'json',
+ *   modelYear: 2009
+ * }
+ */
+export interface QueryStringParameters {
+  [propName: string]: string | number;
 }
 
 /**
+ * @category utils
  * @async
- * @description Utility method to generate a query string,<br>
- *  for use with an NHTSA.gov API URL string. <br><br>
+ * @module utils/makeQueryString
+ * @description Utility method to generate a query string.<br>
+ *   Prepend it to an API URL string. <br>
+ *   ---
  *
- * @param {object} params <a name="params"></a> `REQUIRED`<br>
- *  #### An object containing key:value API query parameters to use.<br>
  *
- * All params will be converted into strings with
+ * @param {QueryStringParameters} params <a name="params"></a>
+ * An object containing key:value API query parameters to use.<br>
+ * Parameter values may be either type 'string' or type 'number'
  *
  * @returns {Promise<string>|Error} An API query string <br>
  *  - On resolve: `Promise(<string>)`<br>
- *  - On reject: `new Error(error<string>)`
+ *  - On reject: `new Error(<string>)` - if parameters are not of type 'object'
  *
  * @example <caption>Single Param:</caption>
  * const qs = await genQueryString({
  *   format: 'json'
  * }).catch(error => error)
- * // => Promise.resolve("?format=json")
+ * //  qs = "format=json"
  *
  * @example <caption>Multiple Params:</caption>
  * const qs = await genQueryString({
@@ -31,22 +45,24 @@ export interface Params {
  *   modelYear: 2006,
  *   page: "2"
  * }).catch(error => error)
- * // => Promise.resolve("?format=json&modelYear=2006&page=2")
+ * // qs = "?format=json&modelYear=2006&page=2"
  *
  * @example <caption>Empty Params Object:</caption>
  * const qs = await genQueryString({}).catch(error => error)
- * // => Promise.resolve("")
+ * // qs = ""
  *
  */
 
-export function queryString(params: Params = {}): Promise<string> {
+export function makeQueryString(
+  params: QueryStringParameters = {}
+): Promise<string> {
   // Error message begins with
   const errorBase =
-    'queryString(params) - expected params in the form of an object ';
+    'queryString(params) - expected params in the form of an object , got:';
 
   // Type guard params argument
   if (Object.prototype.toString.call(params) !== '[object Object]') {
-    return Promise.reject(new Error(`${errorBase} got: ${params}`));
+    return Promise.reject(new Error(`${errorBase} ${params}`));
   }
 
   // Setup QueryString for Array mapping
@@ -56,7 +72,7 @@ export function queryString(params: Params = {}): Promise<string> {
   // Return an empty string if params are an empty object
   if (paramsLength < 1) return Promise.resolve('');
 
-  // Used to check if we've already prepended a first valid query param
+  // Used to check if we've already prepended a valid query param
   let isPrepended = false;
 
   // Map [key]:value entries to "key=value" strings in an array
@@ -64,8 +80,14 @@ export function queryString(params: Params = {}): Promise<string> {
     let prepend = '';
     let append = '';
 
-    // skip any invalid values, string or number types are valid
-    if (value && (typeof value === 'string' || typeof value === 'number')) {
+    const typeofValue = typeof value;
+
+    if (typeofValue === 'number') {
+      value = value.toString();
+    }
+
+    // skip any invalid values, only string or number types are valid
+    if (value && (typeofValue === 'string' || typeofValue === 'number')) {
       // if this is the first param we need to prepend the '?' char
       if (value !== '' && !isPrepended) {
         prepend = '?';
@@ -73,10 +95,7 @@ export function queryString(params: Params = {}): Promise<string> {
       }
       // if there is another param coming after this one we need to append the '&' char
       if (index < paramsLength - 1) {
-        // but only if the value is not empty
-        if (entries[index + 1][1] !== '') {
-          append = '&';
-        }
+        append = '&';
       }
 
       // map the completed partial query string to queryStringArray
