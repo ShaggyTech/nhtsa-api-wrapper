3d2a119105262b264ce7709fac1ed1c2e6bcec41
ShaggyTech <beedat1@gmail.com>
2020-02-06 22:25:38 -0600

    TESTS: removed greet tests

--- src/__tests__/index.test.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable node/no-missing-import */
-
-import { greet } from '../index';
-
-test('My Greeter', () => {
-  expect(greet('Carl', 31)).toBe('Hello Carl, you are 31 years old!');
-});
