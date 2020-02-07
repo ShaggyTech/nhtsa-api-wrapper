cfa3a84ee9c2c0299a25d9cce6b867a109355c93
ShaggyTech <beedat1@gmail.com>
2020-02-06 22:34:46 -0600

    TESTS: API, NHTSA and Client import/exports

---
+++ src/api/__tests__/api-index.test.ts
@@ -0,0 +1,13 @@
+import { NHTSA, Client } from '../index';
+
+describe('NHTSA Class', () => {
+  test('it exists', () => {
+    expect(NHTSA).toBeDefined();
+  });
+});
+
+describe('NHSTA Client', () => {
+  test('it exists', () => {
+    expect(Client).toBeDefined();
+  });
+});
