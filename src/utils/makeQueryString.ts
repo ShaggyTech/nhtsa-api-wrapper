baacb1ed351ab7f1343835036a2e0b551f93ce10
ShaggyTech <beedat1@gmail.com>
2020-02-09 16:30:53 -0600

    UTILS: makeQueryString(), cleanup dev comments

--- src/utils/makeQueryString.ts
+++ src/utils/makeQueryString.ts
@@ -27,16 +27,13 @@ export interface QueryStringParameters {
  * @async
  * @description Utility method to generate a query string.<br>
  *   Prepend it to an API URL string. <br>
- *   ---
  *
  * @param {object} params Object of Type [QueryStringParameters](module-makeQueryString.QueryStringParameters.html)
  * @param {boolean} allowEmptyStringValues=false
  *   Set to `true` to add empty parameter values to the returned query string.
  *   GetCanadianVehicleSpecifications is the only API Action that requires this functionality.
  *
- * @returns {Promise<string>|Error} An API query string <br>
- *  - On resolve: `Promise(<string>)`<br>
- *  - On reject: `new Error(<string>)` - if parameters are not of type 'object'
+ * @returns {Promise<string>|Error} An API query string for use in a final URL.
  *
  * @example <caption>Single Param:</caption>
  * const qs = await makeQueryString({
@@ -70,23 +67,23 @@ export function makeQueryString(
   params: QueryStringParameters = {},
   allowEmptyStringValues = false
 ): Promise<string | Error> {
-  // Error message begins with
+  /* Beginning of error message string */
   const errorBase =
     'queryString(params) - expected params in the form of an object, got:';
 
-  // Type guard params argument, must be of type object
+  /* Runtime type guard params argument, must be of type object */
   if (Object.prototype.toString.call(params) !== '[object Object]') {
     return Promise.reject(new Error(`${errorBase} ${params}`));
   }
 
-  // Setup QueryString for Array mapping
+  /* Setup QueryString for Array mapping */
   const entries = Object.entries(params);
   const paramsLength = entries.length;
 
-  // Return an empty string if params are an empty object
+  /* Return an empty string if params are an empty object */
   if (paramsLength < 1) return Promise.resolve('');
 
-  // Used to check if we've already prepended a valid query param
+  /* Used to check if we've already prepended a valid query param */
   let isPrepended = false;
 
   // Map [key]:value entries to "key=value" strings in an array
@@ -100,27 +97,27 @@ export function makeQueryString(
       value = value.toString();
     }
 
-    // skip any invalid values, only string, number, or boolean types are valid
+    /* Skip any invalid values, only string and number value types are valid */
     if (
       (value || allowEmptyStringValues) &&
       (typeofValue === 'string' || typeofValue === 'number')
     ) {
-      // if this is the first param we need to prepend the '?' char
+      /* if this is the first param we need to prepend the '?' char */
       if (!isPrepended) {
         prepend = '?';
         isPrepended = true;
       }
-      // if there is another param coming after this one we need to append the '&' char
+      /* if there is another param coming after this one we need to append the '&' char */
       if (index < paramsLength - 1) {
         append = '&';
       }
 
-      // map the completed partial query string to queryStringArray
+      /* Add the completed partial query string to queryStringArray */
       return `${prepend}${key}=${value}${append}`;
     }
     return;
   });
 
-  // Join and return the completed query string anfter URI encoding
+  /* Join and return the completed query string after URI encoding */
   return Promise.resolve(encodeURI(queryStringArray.join('')));
 }
