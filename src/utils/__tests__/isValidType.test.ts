99ecdd7df4427e4c65151f61b04ebc0b4babbad1
ShaggyTech <beedat1@gmail.com>
2020-02-14 00:04:26 -0600

    TESTS: utils.isValidType, update tests

--- src/utils/__tests__/isValidType.test.ts
+++ src/utils/__tests__/isValidType.test.ts
@@ -1,5 +1,3 @@
-/* eslint-disable node/no-missing-import */
-
 import { isValidType } from '../isValidType';
 
 describe('isValidType utility method', () => {
@@ -7,61 +5,44 @@ describe('isValidType utility method', () => {
     expect(isValidType).toBeDefined();
     expect(isValidType).toBeInstanceOf(Function);
   });
+
   /****************
    * Returns True
    ****************/
-  describe('it returns true when - ', () => {
-    test('value is equal to provided type', async () => {
-      expect(await isValidType({ type: 'String', value: 'x' })).toStrictEqual(
-        true
-      );
-      expect(await isValidType({ type: 'Object', value: {} })).toStrictEqual(
-        true
-      );
-      expect(await isValidType({ type: 'number', value: 31 })).toStrictEqual(
-        true
-      );
-      expect(await isValidType({ type: 'array', value: [] })).toStrictEqual(
-        true
-      );
+  describe('it returns true when -', () => {
+    test('value is equal to provided type', () => {
+      expect(isValidType({ type: 'String', value: 'x' })).toStrictEqual(true);
+      expect(isValidType({ type: 'Object', value: {} })).toStrictEqual(true);
+      expect(isValidType({ type: 'number', value: 31 })).toStrictEqual(true);
+      expect(isValidType({ type: 'array', value: [] })).toStrictEqual(true);
     });
   });
 
   /****************
    * Returns False
    ****************/
-  describe('it returns false when - ', () => {
+  describe('it returns false when -', () => {
+    test('value is not equal to provided type', async () => {
+      expect(isValidType({ type: 'string', value: 31 })).toStrictEqual(false);
+      expect(isValidType({ type: 'object', value: 'no' })).toStrictEqual(false);
+      expect(isValidType({ type: 'Number', value: {} })).toStrictEqual(false);
+      expect(isValidType({ type: 'Array', value: '' })).toStrictEqual(false);
+    });
+
     test('no arguments provided', async () => {
-      expect(await isValidType(undefined as any)).toStrictEqual(false);
+      expect(isValidType(undefined as any)).toStrictEqual(false);
     });
 
     test('type is empty string', async () => {
-      expect(await isValidType({ type: '', value: 31 })).toStrictEqual(false);
+      expect(isValidType({ type: '', value: 31 })).toStrictEqual(false);
     });
 
     test('value of type is not a string', async () => {
-      expect(await isValidType({ type: 21 as any, value: 31 })).toStrictEqual(
-        false
-      );
+      expect(isValidType({ type: 21 as any, value: 31 })).toStrictEqual(false);
     });
 
     test('value key is missing', async () => {
-      expect(await isValidType({ type: 'string' } as any)).toStrictEqual(false);
-    });
-
-    test('value is not equal to provided type', async () => {
-      expect(await isValidType({ type: 'string', value: 31 })).toStrictEqual(
-        false
-      );
-      expect(await isValidType({ type: 'object', value: 'no' })).toStrictEqual(
-        false
-      );
-      expect(await isValidType({ type: 'Number', value: {} })).toStrictEqual(
-        false
-      );
-      expect(await isValidType({ type: 'Array', value: '' })).toStrictEqual(
-        false
-      );
+      expect(isValidType({ type: 'string' } as any)).toStrictEqual(false);
     });
   });
 });
