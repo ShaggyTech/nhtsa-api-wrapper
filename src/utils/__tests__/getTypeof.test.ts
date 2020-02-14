83dd9e91829a907d0a716011b7147d69202c6e55
ShaggyTech <beedat1@gmail.com>
2020-02-14 00:03:21 -0600

    TESTS: utils.getTypeof(), change toEqual to toStrickEqual in expects assertions

--- src/utils/__tests__/getTypeof.test.ts
+++ src/utils/__tests__/getTypeof.test.ts
@@ -7,12 +7,12 @@ describe('getTypeof utility helper function', () => {
   });
 
   test('it returns correct type', () => {
-    expect(getTypeof(undefined)).toEqual('undefined');
-    expect(getTypeof(null)).toEqual('null');
-    expect(getTypeof(true)).toEqual('boolean');
-    expect(getTypeof('this is a string')).toEqual('string');
-    expect(getTypeof(() => 'this is a function')).toEqual('function');
-    expect(getTypeof({ an: 'object' })).toEqual('object');
-    expect(getTypeof(['an', 'array'])).toEqual('array');
+    expect(getTypeof(undefined)).toStrictEqual('undefined');
+    expect(getTypeof(null)).toStrictEqual('null');
+    expect(getTypeof(true)).toStrictEqual('boolean');
+    expect(getTypeof('this is a string')).toStrictEqual('string');
+    expect(getTypeof(() => 'this is a function')).toStrictEqual('function');
+    expect(getTypeof({ an: 'object' })).toStrictEqual('object');
+    expect(getTypeof(['an', 'array'])).toStrictEqual('array');
   });
 });
