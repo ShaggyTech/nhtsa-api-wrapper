d7b78da468ca1ac0c8b2591de9251f1347ad8957
ShaggyTech <beedat1@gmail.com>
2020-02-15 22:40:50 -0600

    API Actions: GetCanadianVehicleSpecifications, refactor default params values and add correct description in jsdoc comments

--- src/api/actions/GetCanadianVehicleSpecifications.ts
+++ src/api/actions/GetCanadianVehicleSpecifications.ts
@@ -11,16 +11,15 @@ import { Fetch } from '../Fetch';
  */
 export class GetCanadianVehicleSpecifications extends Fetch {
   /**
-   * This returns the Models in the vPIC dataset for a specified Model Year and Make whose name is LIKE the Make in vPIC Dataset.
-   * - `params.year` is the only query parameter the user must provide.
-   * - `params.makeId` is a number
-   * - `params.modelYear` is a number (greater than 1995)
-   * - `params.vehicleType` can be a partial name, or a full name for more specificity
-   * (e.g., "Vehicle", "Moto", "Low Speed Vehicle", etc.).
+   * The Canadian Vehicle Specifications (CVS) consists of a database of original vehicle dimensions,
+   * used primarily in collision investigation and reconstruction, combined with a search engine.
+   * The database is compiled annually by the Collision Investigation and Research Division of Transport Canada.
+   * Visit official [Canadian Vehicle Specifications](http://www.carsp.ca/research/resources/safety-sources/canadian-vehicle-specifications/)
+   * page for more details.
    *
    * This API action will return a 404 html error if any of the query parameters in params
-   *   are missing from the query string. This is the only API action with this behaviour. Therefore,
-   *   parameters are inserted into the query string as empty strings, if not provided by the user.
+   * are missing from the query string. This is the only API action with this behaviour. Therefore,
+   * parameters are inserted into the query string as empty strings if not provided by the user.
    *
    * @async
    * @param {object} params - Query Search Parameters to append to the URL.
@@ -42,7 +41,7 @@ export class GetCanadianVehicleSpecifications extends Fetch {
   ): Promise<import('../types').ApiResponse | Error> {
     const action = 'GetCanadianVehicleSpecifications';
 
-    /* Runtime gatekeeping */
+    /* Runtime typechecking */
     if (!params.year) {
       return Promise.reject(
         new Error(`${action}, params.year is required, got: ${params.year}`)
@@ -50,19 +49,27 @@ export class GetCanadianVehicleSpecifications extends Fetch {
     }
 
     /* Set default query parameters to empty strings if not provided by the user */
-    if (!params.make) params.make = '';
-    if (!params.model) params.model = '';
-    if (!params.units) params.units = '';
+    const make = params.make || '';
+    const model = params.model || '';
+    const units = params.units || '';
+
+    const params_ = {
+      year: params.year,
+      make,
+      model,
+      units
+    };
 
     /*
-     * Build the 'default' query string to be appended to the URL
+     * Build the 'default' query string to be appended to the URL.
+     *
      * Additionally, set the allowEmptyStringValues option (2nd argument) to true
      * This API action will return a 404 error if any of the query parameters in params
      * are missing from the query string. This is the only API action with this behaviour.
      * "year" is the only param the user must provide.
      */
     const queryString = await this.buildQueryString(
-      params,
+      params_,
       true
     ).catch((err: Error) =>
       Promise.reject(
