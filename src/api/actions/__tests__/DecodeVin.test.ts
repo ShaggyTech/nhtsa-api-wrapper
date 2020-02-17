916d5df674e5f21ab15060e6c16a6e682dea4efd
ShaggyTech <beedat1@gmail.com>
2020-02-17 17:36:47 -0600

    TESTS: DecodeVin, clean up some linting errors and slight refactor

--- src/api/actions/__tests__/DecodeVin.test.ts
+++ src/api/actions/__tests__/DecodeVin.test.ts
@@ -5,23 +5,31 @@ import mockCrossFetch from 'cross-fetch';
 
 import mockData from '../../../__mocks__/mockData';
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
+const getClassInstance = () => {
+  return new DecodeVin();
+};
 
 describe('NHTSA.DecodeVin()', () => {
+  let client: DecodeVin = getClassInstance();
+
+  beforeEach(() => {
+    client = getClassInstance();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('it decodes a VIN', async () => {
-    const client = new DecodeVin();
     const response = await client
       .DecodeVin('3VWD07AJ5EM388202')
       .catch(err => err);
 
     expect(mockCrossFetch).toHaveBeenCalledTimes(1);
-    expect(response).toEqual(mockData);
+    expect(response).toStrictEqual(mockData);
   });
 
   test('it decodes a VIN and handles params', async () => {
-    const client = new DecodeVin();
     const response = await client
       .DecodeVin('3VWD07AJ5EM388202', {
         modelYear: 2001
@@ -29,15 +37,14 @@ describe('NHTSA.DecodeVin()', () => {
       .catch(err => err);
 
     expect(mockCrossFetch).toHaveBeenCalledTimes(1);
-    expect(response).toEqual(mockData);
+    expect(response).toStrictEqual(mockData);
   });
 
   test('it returns an Error when no VIN argument is provided', async () => {
-    const client = new DecodeVin();
     const response = await client.DecodeVin(undefined as any).catch(err => err);
 
     expect(mockCrossFetch).toHaveBeenCalledTimes(0);
-    expect(response).toEqual(
+    expect(response).toStrictEqual(
       Error(
         'DecodeVin, vin argument is required and must be a string, got: undefined'
       )
@@ -45,13 +52,12 @@ describe('NHTSA.DecodeVin()', () => {
   });
 
   test('it returns an Error when invalid typeof VIN argument is provided', async () => {
-    const client = new DecodeVin();
     const response = await client
       .DecodeVin([{ fails: 'should fail' }] as any)
       .catch(err => err);
 
     expect(mockCrossFetch).toHaveBeenCalledTimes(0);
-    expect(response).toEqual(
+    expect(response).toStrictEqual(
       Error(
         'DecodeVin, vin argument is required and must be a string, got: [object Object]'
       )
@@ -63,14 +69,13 @@ describe('NHTSA.DecodeVin()', () => {
       .spyOn(Fetch.prototype, 'buildQueryString')
       .mockImplementationOnce(() => Promise.reject('mock error'));
 
-    const client = new DecodeVin();
     const response = await client
       .DecodeVin('3VWD07AJ5EM388202')
       .catch(err => err);
 
     expect(mockCrossFetch).toHaveBeenCalledTimes(0);
     expect(client.buildQueryString).toHaveBeenCalledTimes(1);
-    expect(response).toEqual(
+    expect(response).toStrictEqual(
       Error('DecodeVin, Error building query string: mock error')
     );
   });
@@ -80,12 +85,13 @@ describe('NHTSA.DecodeVin()', () => {
       .spyOn(Fetch.prototype, 'get')
       .mockImplementationOnce(() => Promise.reject('mock error'));
 
-    const client = new DecodeVin();
     const response = await client
       .DecodeVin('3VWD07AJ5EM388202')
       .catch(err => err);
 
     expect(client.get).toHaveBeenCalledTimes(1);
-    expect(response).toEqual(Error('DecodeVin, Fetch.get() error: mock error'));
+    expect(response).toStrictEqual(
+      Error('DecodeVin, Fetch.get() error: mock error')
+    );
   });
 });
