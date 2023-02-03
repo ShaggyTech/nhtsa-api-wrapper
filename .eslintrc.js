56bff869baab3a1a3e39ca2624025a0ab53fe2d4
shaggytech <beedat1@gmail.com>
2023-02-02 23:45:24 -0600

    chore(monorepo): use proper tsconfig logic, use tsconfig paths '@' in packages/lib, remove eslint/prettier configs and dependencies from root of monorepo

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['custom']
-  // https://typescript-eslint.io/linting/typed-linting/monorepos/
-  // parserOptions: {
-  //   project: ['./apps/*/tsconfig.json', , './packages/*/tsconfig.json'],
-  //   tsconfigRootDir: __dirname
-  // }
-}
--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -2,12 +2,18 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// variable to store if this is dev mode
+const isDevMode = process.env.NODE_ENV === 'development'
+const dtsOutDir = isDevMode ? 'dev/dist/types' : 'dist/types'
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     dts({
       entryRoot: './src/',
-      outputDir: './dist/types/',
+      outputDir: dtsOutDir,
       insertTypesEntry: true,
     }),
   ],
