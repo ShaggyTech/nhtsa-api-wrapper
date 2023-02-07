fc757f83aa479e6d5514786a4b7ec51ad4305d79
ShaggyTech <beedat1@gmail.com>
2023-02-07 17:59:46 -0600

    docs: add boilerplate for vitepress documentation

---
+++ apps/documentation/src/.vitepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: '@shaggytools/nhtsa-api-wrapper',
+  titleTemplate: ":title - Javascript Wrapper for the NHTSA VPIC API",
+  description: 'Documentation website for @shaggytools/nhtsa-api-wrapper',
+  appearance: 'dark',
+  base: '/nhtsa-api-wrapper/',
+})
+
