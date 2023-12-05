749394e50cea4062e7fb2c9492dfd31f998bbd01
shaggytech <beedat1@gmail.com>
2023-12-05 01:01:59 -0600

    refactor(package): explicitly name exported types in src index

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @module exports
+ * @category Package Exports
+ */
+
+/**
+ * @category Exported NHTSA API Functions
+ */
 export {
   DecodeVin,
   DecodeVinExtended,
@@ -26,6 +34,41 @@ export {
   useNHTSA,
 } from '@/api'
 
+/**
+ * @category Exported Utility Functions
+ */
 export { createQueryString, encodeQueryStringParams, isValidVin } from '@/utils'
 
-export * from '@/types'
+/**
+ * @category Exported Types
+ */
+export type {
+  DecodeVinExtendedResults,
+  DecodeVinResults,
+  DecodeVinValuesBatchResults,
+  DecodeVinValuesExtendedResults,
+  DecodeVinValuesResults,
+  DecodeVinExtendedVariable,
+  DecodeVinVariable,
+  GetCanadianVehicleSpecificationsResults,
+  GetEquipmentPlantCodesResults,
+  GetMakeForManufacturerResults,
+  DecodeWMIResults,
+  GetAllMakesResults,
+  GetAllManufacturersResults,
+  GetEquipmentPlantCodesParams,
+  GetMakesForManufacturerAndYearResults,
+  GetMakesForVehicleTypeResults,
+  GetManufacturerDetailsResults,
+  GetModelsForMakeIdResults,
+  GetModelsForMakeIdYearResults,
+  GetModelsForMakeResults,
+  GetModelsForMakeYearResults,
+  GetPartsResults,
+  GetVehicleTypesForMakeIdResults,
+  GetVehicleTypesForMakeResults,
+  GetVehicleVariableListResults,
+  GetVehicleVariableValuesListResults,
+  GetWMIsForManufacturerResults,
+  NhtsaResponse,
+} from '@/types'
