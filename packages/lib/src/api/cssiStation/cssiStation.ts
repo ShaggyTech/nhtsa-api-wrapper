37c346c55712cd99eb780bd3e4985380a6d4a034
shaggytech <beedat1@gmail.com>
2023-12-26 00:14:38 -0600

    feat(lib): add function wrapper for CSSIStation API

--- packages/lib/src/api/cssiStation/cssiStation.ts
+++ packages/lib/src/api/cssiStation/cssiStation.ts
@@ -3,6 +3,18 @@
  * @category API - CSSI Station Locator API
  */
 
+import { useNHTSA } from '@/api'
+import {
+  catchInvalidArguments,
+  encodeQueryStringParams,
+  rejectWithError,
+} from '@/utils'
+import type {
+  NhtsaResponse,
+  NoExtraProperties,
+  QueryStringParamsEncoded,
+} from '@/types'
+
 /**
 Rough Draft Notes:
 
@@ -91,7 +103,7 @@ Possible Options Combinations:
 - location + filters (same as location only)
 
 Rules:
-- CSSIStation is not case sensitive for the path name
+- CSSIStation is not case sensitive for the path name, can be /CSSIStation or /cssistation, etc.
 - If zip is provided, cannot provide state or location
 - If state is provided, cannot provide zip or location
 - lat, long, and miles must be provided all or none
@@ -144,7 +156,8 @@ cssiStation({ state: 'NV', filters: { lang: 'spanish' } })
 
 LOCATION (lat, long, miles):
 - returns first 100 stations, same as no options
-- query string has no effect on returned data
+- query string has no effect on returned data, returns same data as no query string
+- these options appears to be broken or abandoned by NHTSA
 - uses base url of /CSSIStation with query string of ?lat=32.71325&long=-97.28864&miles=50
 - filters are added to query string as &lang=spanish &cpsweek=true
 
@@ -344,3 +357,368 @@ Needs more real world testing before we try to implement.
 /**
  * # CSSI Station
  */
+function cssiStation(
+  options: CSSIOptions,
+  doFetch?: true
+): Promise<CSSIResponse>
+function cssiStation(
+  options: CSSIOptions | undefined,
+  doFetch: false
+): Promise<string>
+function cssiStation(options?: undefined, doFetch?: true): Promise<CSSIResponse>
+function cssiStation(doFetch?: true): Promise<CSSIResponse>
+function cssiStation(doFetch: false): Promise<string>
+function cssiStation(
+  options?: CSSIOptions | boolean,
+  doFetch?: boolean
+): Promise<unknown>
+/* Implementation */
+async function cssiStation(
+  options?: CSSIOptions | boolean,
+  doFetch: boolean = true
+): Promise<unknown> {
+  const endpointName = 'CSSIStation'
+  const apiType = 'cssiStation'
+
+  /**
+   * Builds the URL path
+   */
+  const buildPath = (parts: CSSIOptionsBase): string => {
+    const pathParts = []
+    if (parts.state) {
+      pathParts.push(`state/${parts.state}`)
+      if (parts.filters?.lang) {
+        pathParts.push(`lang/${parts.filters.lang}`)
+      }
+      // filters.cpsweek not compatible with state
+    }
+    if (parts.zip) {
+      pathParts.push(`zip/${parts.zip}`)
+      if (parts.filters?.cpsweek) {
+        pathParts.push('cpsweek')
+      } else if (parts.filters?.lang) {
+        pathParts.push(`lang/${parts.filters.lang}`)
+      }
+      // if (zip && filters.cpsweek && filters.lang) - lang will be added as query string
+    }
+    return pathParts.join('/')
+  }
+
+  try {
+    let path = ''
+    let encodedParams: QueryStringParamsEncoded<CSSIOptionsCanBeQuery> = {}
+
+    if (typeof options === 'boolean') {
+      /* If first argument is boolean, it is doFetch */
+      doFetch = options
+      /* Set options undefined so it will pass argument check below, otherwise invalid type */
+      options = undefined
+    }
+
+    /* Throw an error if options contains invalid keys or types of values */
+    catchInvalidArguments({
+      args: [
+        {
+          name: 'options',
+          value: options,
+          types: ['object'],
+          validKeys: ['state', 'zip', 'location', 'filters'],
+        },
+        {
+          name: 'state',
+          value: options?.state,
+          types: ['string'],
+        },
+        {
+          name: 'zip',
+          value: options?.zip,
+          types: ['string', 'number'],
+        },
+        {
+          name: 'location',
+          value: options?.location,
+          types: ['object'],
+          validKeys: ['lat', 'long', 'miles'],
+        },
+        {
+          name: 'location.lat',
+          value: options?.location?.lat,
+          types: ['string', 'number'],
+          requiredBy: [
+            { name: 'location.long', value: options?.location?.long },
+            { name: 'location.miles', value: options?.location?.miles },
+          ],
+        },
+        {
+          name: 'location.long',
+          value: options?.location?.long,
+          types: ['string', 'number'],
+          requiredBy: [
+            { name: 'location.lat', value: options?.location?.lat },
+            { name: 'location.miles', value: options?.location?.miles },
+          ],
+        },
+        {
+          name: 'location.miles',
+          value: options?.location?.miles,
+          types: ['string', 'number'],
+          requiredBy: [
+            { name: 'location.lat', value: options?.location?.lat },
+            { name: 'location.long', value: options?.location?.long },
+          ],
+        },
+        {
+          name: 'filters',
+          value: options?.filters,
+          types: ['object'],
+          validKeys: ['lang', 'cpsweek'],
+        },
+        {
+          name: 'filters.lang',
+          value: options?.filters?.lang,
+          types: ['string'],
+        },
+        {
+          name: 'filters.cpsweek',
+          value: options?.filters?.cpsweek,
+          types: ['boolean'],
+        },
+      ],
+    })
+
+    /* options are guaranteed to be an object by now because of catchInvalidArguments() */
+    if (options) {
+      const {
+        state,
+        zip,
+        location: { lat, long, miles } = {},
+        filters: { lang, cpsweek } = {},
+      } = options
+      const hasLocation = lat && long && miles
+      const hasBothFilters = cpsweek && lang
+
+      /* Throw an error if state, zip, and location are provided together in any combination */
+      if (
+        (state && zip && hasLocation) ||
+        (state && zip) ||
+        (state && hasLocation) ||
+        (zip && hasLocation)
+      ) {
+        throw new Error(
+          `Invalid options: state, zip, and location are not compatible.`
+        )
+      }
+
+      /* Throw an error if cpsweek is included with state */
+      if (state && cpsweek) {
+        throw new Error(
+          `Invalid options: state and filters.cpsweek are not compatible.`
+        )
+      }
+
+      /* Build the API URL path with state or zip, some filters used in path */
+      if (state || zip) {
+        path = buildPath(options)
+      }
+      /* lang is a query when used with zip and filters.cpsweek */
+      if (zip && hasBothFilters) {
+        encodedParams = encodeQueryStringParams({ lang })
+      }
+      /* location uses lat, long, miles, and possible filters as query string */
+      if (hasLocation) {
+        encodedParams = encodeQueryStringParams({
+          lat,
+          long,
+          miles,
+          lang,
+          cpsweek,
+        })
+      }
+    }
+
+    const { get, createCachedUrl, getCachedUrl } = useNHTSA()
+
+    createCachedUrl({
+      apiType,
+      endpointName,
+      path,
+      params: encodedParams,
+    })
+
+    if (!doFetch) {
+      return getCachedUrl()
+    } else {
+      return get<CSSIResultsData, typeof apiType>()
+    }
+  } catch (error) {
+    return rejectWithError(error)
+  }
+}
+
+export { cssiStation }
+
+/**
+ * These types have to be kept together with the function in the same file.
+ *
+ * This is so Intellisense will show the full type contents of the return to the end user when they
+ * save the results to a variable and then hover over that variable. If these types are moved to
+ * another file, Intellisense will only show the type name, not the full type contents for the
+ * cssiStation() NhstaResponse return type.
+ *
+ * Any type that is used directly in the function overloads and not in this file will cause this.
+ * So, theoretically, you could move all of those types here and the others live in their own file,
+ * but that would spread the types out and they are only used here and in the tests, so it makes
+ * sense to keep them together.
+ */
+
+/**
+ * All valid options for the `cssiStation()` function
+ */
+export type CSSIOptionsBase = {
+  state?: string
+  zip?: string | number
+  location?: {
+    lat: string | number
+    long: string | number
+    miles: string | number
+  }
+  filters?: {
+    lang?: string
+    cpsweek?: boolean
+  }
+}
+
+/**
+ * Options that can be used as query string params
+ * - `lang` is a only query when used with `CSSIOptionsZip`
+ * - `cpsweek` is a only query when used with `CSSIOptionsLocation`
+ * - `lat`, `long`, and `miles` are only a query when used with `CSSIOptionsLocation`
+ * - `lang` and `cpsweek` are also a query when used with `CSSIOptionsLocation`
+ */
+export type CSSIOptionsCanBeQuery = {
+  lat?: string | number
+  long?: string | number
+  miles?: string | number
+  lang?: string
+  cpsweek?: boolean
+}
+
+/**
+ * Options to get the first 100 stations in the database.
+ *
+ * Builds path: `/CSSIStation`
+ */
+export type CSSIOptionsEmpty = {
+  state?: undefined
+  zip?: undefined
+  location?: undefined
+  filters?: undefined
+}
+
+/**
+ * Options to get all stations for a `state` passed to the `products()` function with an optional
+ * `lang` filter.
+ *
+ * Builds path: `/CSSIStation/state/${state}` or `/CSSIStation/state/${state}/lang/${lang}`
+ */
+export type CSSIOptionsState = {
+  state: string
+  filters?: {
+    lang?: string
+    cpsweek?: undefined
+  }
+  zip?: undefined
+  location?: undefined
+}
+
+/**
+ * Options to get all stations for a `zip` passed to the `products()` function with optional
+ * `lang` and `cpsweek` filters.
+ *
+ * Builds paths:
+ * - `/CSSIStation/zip/${zip}`
+ * - `/CSSIStation/zip/${zip}/lang/${lang}`
+ * - `/CSSIStation/zip/${zip}/cpsweek`
+ * - `/CSSIStation/zip/${zip}/cpsweek?lang=${lang}`
+ */
+export type CSSIOptionsZip = {
+  zip: string | number
+  filters?: {
+    lang?: string
+    cpsweek?: boolean
+  }
+  state?: undefined
+  location?: undefined
+}
+
+/**
+ * Options to get all stations for a location `lat`, `long`, and `miles` passed to the
+ * `products()` function, with optional `lang` and `cpsweek` filters.
+ *
+ * Builds paths:
+ * - `/CSSIStation?lat=${lat}&long=${long}&miles=${miles}`
+ * - `/CSSIStation?lat=${lat}&long=${long}&miles=${miles}&lang=${lang}`
+ * - `/CSSIStation?lat=${lat}&long=${long}&miles=${miles}&cpsweek=true`
+ * - `/CSSIStation?lat=${lat}&long=${long}&miles=${miles}&cpsweek=true&lang=${lang}`
+ */
+export type CSSIOptionsLocation = {
+  location: {
+    lat: string | number
+    long: string | number
+    miles: string | number
+  }
+  filters?: {
+    lang?: string
+    cpsweek?: boolean
+  }
+  state?: undefined
+  zip?: undefined
+}
+
+/**
+ * All valid options combinations for the `cssiStation()` function
+ */
+export type CSSIOptions =
+  | CSSIOptionsEmpty
+  | CSSIOptionsState
+  | CSSIOptionsZip
+  | CSSIOptionsLocation
+
+/**
+ * Ensures only valid options are passed to the `cssiStation()` function
+ */
+export type NoInvalidOptions<T extends CSSIOptions> = NoExtraProperties<
+  CSSIOptionsBase,
+  T
+>
+
+/**
+ * All possible properties and their value types, found in the `Results[]` objects of the
+ * `CSSI Station API` response. This is typed with all properties optional so it can be used to
+ * type the `Results[]` based on `options` passed to the `cssiStation()` function.
+ */
+export type CSSIResultsData = {
+  ContactFirstName: string | null
+  ContactLastName: string | null
+  Organization: string | null
+  AddressLine1: string | null
+  City: string | null
+  State: string | null
+  Zip: string | null
+  Email: string | null
+  Fax: string | null
+  Phone1: string | null
+  CPSWeekEventFlag: string | null
+  LastUpdatedDate: string | null
+  MobileStationFlag: string | null
+  CountiesServed: string | null
+  LocationLatitude: number | null
+  LocationLongitude: number | null
+}
+
+/**
+ * Types the return of the `products()` function.
+ *
+ * The `Results[]` will be typed as `CSSIResultsData`
+ */
+export type CSSIResponse = NhtsaResponse<CSSIResultsData, 'cssiStation'>
